b1e469acf59b77cb1398e66427fead1d3486e98b
ysknsid25 <yoshida-kengo@kcgrp.jp>
2025-11-06 18:52:11 +0900

    reference
    Signed-off-by: ysknsid25 <yoshida-kengo@kcgrp.jp>

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
-fn myprint<T: std::fmt::Display>(val: T) {
+fn myprint<T: std::fmt::Display>(val: &T) {
     println!("{}", val);
 }
 
 fn main() {
-    // let x = "hello".to_string();
-    // myprint(x);
-    // myprint(x); // エラー: xはすでにムーブされている
-    
     let x = "hello".to_string();
-    let xx = x.clone();
-    myprint(x);
-    myprint(xx); // エラー: xはすでにムーブされている
+    myprint(&x);
+    myprint(&x);
 }
