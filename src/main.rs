71fb5575d48d005c97c521dc68cfc0900ed5dd2f
ysknsid25 <yoshida-kengo@kcgrp.jp>
2025-11-06 18:31:04 +0900

    ミュータブルvsイミュータブル
    Signed-off-by: ysknsid25 <yoshida-kengo@kcgrp.jp>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
-#[path="module_abc.rs"]
-mod module_hello;
-
 fn main() {
-    module_hello::print_hello();
+    let x = 1;
+    println!("Value of x is: {}", x);
+
+    // x += 1; これは出来ない
+
+    let mut y = 1;
+    println!("Value of y is: {}", y);
+
+    y += 1;
+    println!("Value of y after increment is: {}", y);
+
 }
